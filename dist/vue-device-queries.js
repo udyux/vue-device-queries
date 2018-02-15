7f34e96b85359f454fe4e32b25ed487f997d8396
Nicolas Udy <dev@udy.io>
2018-02-15 01:42:07 -0500

    Update version

--- dist/vue-device-queries.js
+++ dist/vue-device-queries.js
@@ -1,4 +1,4 @@
-/*! portal-vue v1.0.7 | Licence: MIT (c) Nicolas Udy */
+/*! portal-vue v1.0.8 | Licence: MIT (c) Nicolas Udy */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define('VueDeviceQueries', factory) :
